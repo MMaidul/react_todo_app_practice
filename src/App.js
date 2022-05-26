0a1711832f99dfafb6ae2887aea9e8593c26fa2c
MMaidul <maidulmuqtadir360@gmail.com>
2022-05-26 14:18:47 +0600

    data collect from data json and show in ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import Card from './components/Card/Card';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Card from './components/Card/Card';
+
+
 
 const App = () => {
+    const [todoLists,setTodoLists] = useState([]);
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>{
+           setTodoLists(data)
+        })
+    },[])
+    console.log(todoLists);
+
     return (
-        <div>
-            <Card/>
+
+        <div className='todoList-container'>
+            <h1 className='headingStyle'>Todo App</h1>
+          {
+              todoLists.map(todoList=><Card
+                  todoList={todoList}
+              />)
+          }
         </div>
     );
 };
